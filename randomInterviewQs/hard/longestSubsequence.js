d0b7c5db181aebfad765f20cd520a9678276dbf2
slittleton <littletonltp@gmail.com>
2019-09-30 14:48:51 -0700

    commit

---
+++ randomInterviewQs/hard/longestSubsequence.js
@@ -0,0 +1,27 @@
+/** 
+ * GOOGLE INTERVIEW QUESTION
+ * 
+ * write a function that takes two strings, s1 and s2
+ * and returns the longest common subsequence of 
+ * s1 and s2.
+ * 
+ * ("ABAZDC", "BACBAD") => "ABAD"
+ * ("AGGTAB", "GXTXAYB") => "GTAB"
+ * ("aaaa", "aa") => "aa"
+ *  ("", "...") => ""
+ */
+
+function subsequence(s1, s2){
+  if(s1.length === 0 || s2.length === 0) return null;
+
+ 
+
+ 
+ 
+ 
+
+ }
+ // console.log(subsequence("", "BACBAD"));
+ console.log(subsequence("ABAZDC", "BACBAD")); // ABAD
+ // console.log(subsequence("AGGTAB", "GXTXAYB"));
+ // console.log(subsequence("aaaa", "aa"));
--- randomInterviewQs/sumAges.js
+++ randomInterviewQs/sumAges.js
@@ -37,18 +37,3 @@ function sumAges(obj) {
 }
 // console.log(sumAges(profile));
 
-/**
- * create function that prints math problems
- * each problem adds two numbers ex: 1 + 4 =
- * the sum of the numbers must be less than a specified 
- * max number
- *
- */
-function mathProbs(probsNum, maxNum) {
-  for (let i = 0; i < probsNum; i++) {
-    let num1 = Math.round(Math.random() * maxNum);
-    let num2 = Math.round(Math.random() * (maxNum - num1))
-    console.log(`${num1} + ${num2} = `)
-  }
-}
-mathProbs(5, 10);
